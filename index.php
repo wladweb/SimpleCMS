dae6e5c5c486f00d7e839d75f70da2a0d32948e8
wladweb <wladwebwork@gmail.com>
2015-12-04 22:30:08 +0300

    Change index.php

--- index.php
+++ index.php
@@ -9,22 +9,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
 header('Content-Type: text/html; charset=utf-8');
+
 require 'vendor/autoload.php';
-/*
-set_include_path(get_include_path() .
-        PATH_SEPARATOR . 'app/model' .
-        PATH_SEPARATOR . 'app/view' .
-        PATH_SEPARATOR . 'app/controller' .
-        PATH_SEPARATOR . 'app');
-
-function simple_loader($name) {
-    $arr_names = explode('\\', $name);
-    $class = end($arr_names);
-    require_once "$class.php";
-}
-
-\spl_autoload_register('SimpleCMS\simple_loader');
-*/
 
 $app = App::getInstance();
 $app->route();
