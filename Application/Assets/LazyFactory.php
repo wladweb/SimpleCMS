d6e38218f42b4dad9a4b7ccc6305b5919fb4026a
wladweb <wladwebwork@gmail.com>
2015-12-14 21:36:38 +0300

    Add lazy factory

---
+++ Application/Assets/LazyFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace SimpleCMS\Application\Assets;
+
+use SimpleCMS\Application\Model\Posts;
+use SimpleCMS\Application\Model\Category;
+use SimpleCMS\Application\Model\BlogInfo;
+use SimpleCMS\Application\Model\Users;
+use SimpleCMS\Application\Model\Comments;
+
+class LazyFactory {
+    
+    private $posts;
+    private $categoty;
+    private $bloginfo;
+    private $users;
+    private $comments;
+
+    public function getPosts() {
+
+        if (!$this->posts) {
+            $this->posts = new Posts;
+        }
+        return $this->posts;
+    }
+    
+    public function getCategory() {
+
+        if (!$this->category) {
+            $this->category = new Category;
+        }
+        return $this->category;
+    }
+    
+    public function getBloginfo() {
+
+        if (!$this->bloginfo) {
+            $this->bloginfo = new BlogInfo;
+        }
+        return $this->bloginfo;
+    }
+    
+    public function getUsers() {
+
+        if (!$this->users) {
+            $this->users = new Users;
+        }
+        return $this->users;
+    }
+    
+    public function getComments() {
+
+        if (!$this->comments) {
+            $this->comments = new Comments;
+        }
+        return $this->comments;
+    }
+
+}
